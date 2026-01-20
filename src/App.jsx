50568e67f11bbce47d1b408aee45a955c6dfbef3
Golden_Reflection <reflectiongolden@gmail.com>
2026-01-20 11:02:09 +0000

    made initial work for the application to link back to react-essentials-site and include tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './App.css';
+
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Tailwind Card Application</h1>
+      <p>This project has been made in conjunction with the <a title='GitHub Pages site' href="https://reflectiongolden.github.io/react-essentials-site/">react-essentials-site</a><a title='GitHub Repository' href="https://github.com/ReflectionGolden/react-essentials-site"><FontAwesomeIcon icon={faGithub} /></a> project as part of Module 6.</p>
+      <Container>
+        <h2>Tailwind-based Responsive Card</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </Container>
     </>
   )
+}
+
+function Container(props) {
+  return (
+    <div className='mt-4 mb-4 rounded-4xl border-3 border-solid border-orange-100'>
+      {props.children}
+    </div>
+  );
 }
 
 export default App
