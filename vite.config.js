961f1dda72dcaffd385f2bef0c9dd55539975d99
Golden_Reflection <reflectiongolden@gmail.com>
2026-01-20 14:16:08 +0000

    Pages fix attempt 2 (set base to repo)

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: "/tailwind-card-app/",
   plugins: [
     react(),
     tailwindcss(),
